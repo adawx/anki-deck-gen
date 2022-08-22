69b04aa8c3cf4376d4d74035a291d40bef27d67e
Alex Dawkes <alex@dawkes.org>
2022-08-22 23:30:04 +0800

    new start point

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,33 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::io::prelude::*;
-use std::env;
-use serde_json;
+use std::path::Path;
+//use std::env;
+use serde::{Deserialize, Serialize};
 
-//Todo List
-//Figure out file structure for audio files
-//Api call & saving audio to disk
-//reading audio file in & adding to deck
-//save to deck
-//do for all entries?
-
-fn main() {
-    read_in_file().unwrap();
+#[derive(Serialize, Deserialize, Debug)]
+struct NoteInfo {
+    front_text: String,
+    romanisation: String,
+    back_text: String,
 }
 
-fn read_in_file() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+#[derive(Serialize, Deserialize, Debug)]
+struct JsonDeck {
+    name: String,
+    tts: bool,
+    api_key: String,
+    entries: Vec<NoteInfo>,
+}
 
-    //TODO: (ad) If no arg provided the prompt user to input a apth
+fn main() {
+    println!("{:#?}", read_in_deck("data/canto_deck.json").unwrap());
+}
 
-    println!("Making deck from file: {}", file_path);
+fn read_in_deck<P: AsRef<Path>>(path: P) -> std::io::Result<JsonDeck> {
+    let json_file = File::open(path)?;
+    let reader = BufReader::new(json_file);
 
-    let json_file = File::open("data/deck.json")?;
-    let mut buf_reader = BufReader::new(json_file);
-    let mut contents = String::new();   
-    buf_reader.read_to_string(&mut contents)?;
+    let json: JsonDeck = serde_json::from_reader(reader).expect("JSON was not well-formatted");
 
-    let json: serde_json::Value =
-    serde_json::from_str(&contents).expect("JSON was not well-formatted");
-
-    //TODO: (ad) Check present fields in json, if missing fillable fields prompt user, else error if blank? maybe only need entries and prompt for rest??
-    //Arguments: deck name, tts on, tts provider, tts key, ...
-
-
-    println!("{:#?}", json);
-    Ok(())
+    Ok(json)
 }
