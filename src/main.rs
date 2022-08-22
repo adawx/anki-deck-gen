b18b5051cad6ea48983edbf5e9ac7bcf8048d79e
Alex Dawkes <alex@dawkes.org>
2022-08-22 23:55:10 +0800

    example deck creation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 //use std::env;
+use genanki_rs::{basic_model, Deck, Error, Note};
 use serde::{Deserialize, Serialize};
+
+// Take an argument for json File
+// Read in json File
+// pass metadata to create deck fn
+//  create model and then deck
+// Map entries to Cards in anki deck
+// output deck
 
 #[derive(Serialize, Deserialize, Debug)]
 struct NoteInfo {
@@ -19,8 +27,22 @@ struct JsonDeck {
     entries: Vec<NoteInfo>,
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("{:#?}", read_in_deck("data/canto_deck.json").unwrap());
+
+    //Example from genanki_rs
+    let mut deck = Deck::new(1234, "Example Deck", "Example Deck containing 2 Flashcards");
+    deck.add_note(Note::new(
+        basic_model(),
+        vec!["What is the capital of France?", "Paris"],
+    )?);
+    deck.add_note(Note::new(
+        basic_model(),
+        vec!["What is the capital of Germany?", "Berlin"],
+    )?);
+    deck.write_to_file("output/output.apkg")?;
+
+    Ok(())
 }
 
 fn read_in_deck<P: AsRef<Path>>(path: P) -> std::io::Result<JsonDeck> {
