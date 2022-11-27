b251d2819275abf507a2436f594f502ed74776aa
Alex Dawkes <alex@dawkes.org>
2022-11-27 22:59:25 +0800

    deck being output from data source

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # anki-deck-gen
-Tool to build Anki Decks from JSON files using Rust.
+
+Tool to build Anki Decks from JSON files using Rust. Using [genanki-rs](https://github.com/yannickfunk/genanki-rs). Not affiliated with the main anki project.
+
+Mostly built for me to learn some rust, but also to help me be less lazy in my cantonese.
+
+
+### MVP Features:
+- CL arguments (--file --output)
+- Custom CSS for anki cards
+- Optional secondary main text
+- Optional secondary back text?
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
+
 //use std::env;
-use genanki_rs::{basic_model, Deck, Error, Note};
+use genanki_rs::{Deck, Error, Field, Model, Note, Template};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 // Take an argument for json File
@@ -15,32 +17,51 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 struct NoteInfo {
     front_text: String,
-    romanisation: String,
+    front_text2: Option<String>,
     back_text: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct JsonDeck {
     name: String,
+    description: String,
     tts: bool,
     api_key: String,
     entries: Vec<NoteInfo>,
 }
 
-fn main() -> Result<(), Error> {
-    println!("{:#?}", read_in_deck("data/canto_deck.json").unwrap());
+fn main() -> Result<(), Box<Error>> {
+    // TODO clap input File
+    let json_deck = read_in_deck("data/canto_deck.json").unwrap();
+    // println!("{:#?}", json_deck);
 
-    //Example from genanki_rs
-    let mut deck = Deck::new(1234, "Example Deck", "Example Deck containing 2 Flashcards");
-    deck.add_note(Note::new(
-        basic_model(),
-        vec!["What is the capital of France?", "Paris"],
-    )?);
-    deck.add_note(Note::new(
-        basic_model(),
-        vec!["What is the capital of Germany?", "Berlin"],
-    )?);
-    deck.write_to_file("output/output.apkg")?;
+    let mut rng = rand::thread_rng();
+
+    let deck_id: usize = rng.gen_range(1..9999999999);
+    let model_id: usize = rng.gen_range(1..9999999999);
+
+    let mut deck = Deck::new(deck_id, &json_deck.name, &json_deck.description);
+
+    let custom_css = ".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n}\n";
+    let my_model = Model::new(
+        model_id,
+        "card model",
+        vec![Field::new("Front Side"), Field::new("Back Side")],
+        vec![Template::new("Card 1")
+            .qfmt("{{Front Side}}")
+            .afmt(r#"{{Front Side}}<hr id="answer">{{Back Side}}"#)],
+    )
+    .css(custom_css);
+
+    for entry in json_deck.entries.iter() {
+        // println!("{:#?}", entry);
+
+        let note = Note::new(my_model.clone(), vec![&entry.front_text, &entry.back_text])?;
+        deck.add_note(note)
+    }
+
+    let path = format!("output/{}.apkg", &json_deck.name);
+    deck.write_to_file(&path)?;
 
     Ok(())
 }
@@ -53,3 +74,5 @@ fn read_in_deck<P: AsRef<Path>>(path: P) -> std::io::Result<JsonDeck> {
 
     Ok(json)
 }
+
+//fn map_deck(json: JsonDeck, model: Model, deck: Deck) {}
